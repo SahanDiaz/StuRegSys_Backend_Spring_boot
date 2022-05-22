000678400cadc567078f5b76e5b07dc76f6cd07d
SahanDiaz <sahandiaz1@gmail.com>
2022-05-22 20:38:18 +0530

    create controller

--- src/main/java/com/stu/stu/repo/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.stu.stu.repo;
-
-import com.stu.stu.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role,String> {
-}
